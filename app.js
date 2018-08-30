857bce2959897710ed930733faa201d98c042f88
Alok Ranjan Singh <alok@tagtaste.com>
2018-08-31 02:34:50 +0530

    Added mining & code refactoring

--- app.js
+++ app.js
@@ -1,29 +1,8 @@
 const { Block } = require('./class/Block');
-
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
+const express = require('express');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -41,21 +20,62 @@ app.use(function(err, req, res, next) {
     res.render('error');
 });
 
-module.exports = app;
-
+const blockArray = [];
 
 let transactions1 = ["i am a bad man 1", "I love blockchain 1"];
 let block1 = new Block(0, transactions1);
-console.log("Bloack1", block1.blockHash);
+blockArray.push(block1);
+console.log("Mining started...1");
+block1.miningBlock(1);
 
 let transactions2 = ["i am a bad man 2", "I love blockchain 2"];
 let block2 = new Block(block1.blockHash, transactions2);
-console.log("Bloack2", block2.blockHash);
+blockArray.push(block2);
+console.log("Mining started...2");
+block2.miningBlock(1);
 
 let transactions3 = ["i am a bad man 3", "I love blockchain 3"];
 let block3 = new Block(block2.blockHash, transactions3);
-console.log("Bloack3", block3.blockHash);
+blockArray.push(block3);
+console.log("Mining started...3");
+block3.miningBlock(1);
 
 let transactions4 = ["i am a bad man 4", "I love blockchain 4"];
 let block4 = new Block(block3.blockHash, transactions4);
-console.log("Bloack4", block4.blockHash);
+blockArray.push(block4);
+console.log("Mining started...4");
+block4.miningBlock(1);
+
+let isValid = isValidChain(1);
+console.log("IsValid", isValid);
+
+
+console.log("Final Chain", (blockArray));
+
+
+function isValidChain(diffculty) {
+    let array = new Array(diffculty);
+    array.fill(0, 0, diffculty);
+    let target = array.toString().replace(/,/g, "");
+    let currentBlock, previousBlock;
+    for (let index = 1; index < blockArray.length; index++) {
+        currentBlock = blockArray[index];
+        previousBlock = blockArray[index - 1];
+
+        if (!(currentBlock.blockHash == currentBlock.calculateHash())) {
+            return false;
+        }
+
+        if (!(previousBlock.blockHash == currentBlock.previousHash)) {
+            return false;
+        }
+
+        if (!(currentBlock.blockHash.substring(0, diffculty) == target)) {
+            return false;
+        }
+
+    }
+    return true;
+}
+
+module.exports = app;
--- class/Block.js
+++ class/Block.js
@@ -1,21 +1,16 @@
 const crypto = require('crypto');
 
 class Block {
+
+
     constructor(previousHash, transactions) {
         this.previousHash = previousHash;
         this.transactions = transactions;
-
-        const secret = 'blockchain';
-        let transactionsString = this.transactions.join('');
-        let transactionsHash = crypto.createHmac('sha256', secret)
-            .update(transactionsString)
-            .digest('hex');
-
-        let hashObject = { transactionsHash: transactionsHash, previousHash: this.previousHash };
-        this.blockHash = crypto.createHmac('sha256', secret)
-            .update(JSON.stringify(hashObject))
-            .digest('hex');
+        this.nonce = 0;
+        this.timestamp = Date.now();
+        this.blockHash = this.calculateHash();
     }
+
 
     get previousHash() {
         return this._previousHash;
@@ -42,5 +37,25 @@ class Block {
     }
 
 }
+
+Block.prototype.calculateHash = function calculateHash() {
+    const secret = 'blockchain';
+    return crypto.createHmac('sha256', secret)
+        .update(JSON.stringify({ transactionsHash: this.transactionsHash, previousHash: this.previousHash, timestamp: this.timestamp }))
+        .digest('hex');
+};
+
+Block.prototype.miningBlock = function miningBlock(diffculty) {
+    let array = new Array(diffculty);
+    array.fill(0, 0, diffculty);
+    let target = array.toString().replace(/,/g, "");
+    console.log("In mining with target", target);
+    while (!(this.blockHash.substring(0, diffculty) == target)) {
+        this.nonce++;
+        this.blockHash = this.calculateHash();
+    }
+    console.log("Block Mined!!", this.blockHash);
+};
+
 
 module.exports = { Block };
